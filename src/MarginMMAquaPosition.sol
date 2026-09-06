d1b582a0bdc16943bdf0db8af8ae313edbc44efe
Y0sefTamer <programmeryosef@gmail.com>
2026-09-06 18:52:21 +0300

    style: format the code

--- src/MarginMMAquaPosition.sol
+++ src/MarginMMAquaPosition.sol
@@ -1,11 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-
 
 import {MarginMMPricing} from "./MarginMMPricing.sol";
 
@@ -23,20 +21,13 @@ contract MarginMMAquaPosition is Ownable {
     /**
      * @dev Constructor sets the owner, the pricing engine, and the asset token.
      */
-    constructor(
-        address _pricingEngine, 
-        address _asset, 
-        address _initialOwner
-    ) Ownable(_initialOwner) {
+    constructor(address _pricingEngine, address _asset, address _initialOwner) Ownable(_initialOwner) {
         require(_pricingEngine != address(0) && _asset != address(0), "Zero address");
         pricingEngine = MarginMMPricing(_pricingEngine);
         asset = IERC20(_asset);
     }
 
-   
     function depositLiquidity(uint256 amount) external {
         asset.safeTransferFrom(msg.sender, address(this), amount);
     }
-
-   
 }
